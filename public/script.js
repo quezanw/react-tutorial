0d6f2a01cb6166362ea6a7e71e93e8d55d33b445
quezan <quezanw@gmail.com>
2018-12-27 13:50:58 -0800

    created clock that updates each second

--- public/script.js
+++ public/script.js
@@ -5,4 +5,17 @@
     const button = React.createElement('button', null, 'Click Me');
     console.log(JSON.stringify(button));
     ReactDOM.render(button, document.getElementById('app'));
+
+    let interval = setInterval(() => {
+        const date = new Date();
+        let hours = Math.floor(date.getUTCHours() / 12);
+        let minutes = (date.getUTCMinutes() < 10 ? "0" + date.getUTCMinutes() : date.getUTCMinutes());
+        let seconds = (date.getUTCSeconds() < 10 ? "0" + date.getUTCSeconds() : date.getUTCSeconds());
+        let period = (date.getUTCHours() > 12 ? "PM" : "AM");
+        let currentTime = `${hours}:${minutes}:${seconds} ${period}`;
+        // console.log(time);
+        let time = React.createElement('p', null, `The time is: ${currentTime}`);
+        ReactDOM.render(time, document.getElementById('app'));
+        // document.getElementById("time").innerHTML = time;
+    }, 1000);
 }
